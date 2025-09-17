c1efb4cd2c21d6eaba1d83e3de4203d33c8cd30e
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-09-17 02:57:01 +0200

    CSS Instability fixed

--- src/Pages/Applications/Applications.tsx
+++ src/Pages/Applications/Applications.tsx
@@ -85,20 +85,29 @@ export default function Applications(){
         })
     })
     }, [offersElements, offersToApplications.length])
+
+    function toggleClass(e: any){
+        const attribute = e.currentTarget.nextElementSibling?.getAttribute("class")
+        attribute === "applications" ? e.currentTarget.nextElementSibling?.setAttribute("class", "applications disappear") : e.currentTarget.nextElementSibling?.setAttribute("class", "applications")
+    }
     
     return <div className="applications-page">
         {findIfUserIsAdmin() ? <AdminHeader /> : <UserHeader />}
         <div className="content">
             {offersToApplications.map((ota) => <div className="offer">
-                <span className="title">{ota.offer.title}</span>
-                <span className="type">{ota.offer.type}</span>
-                <span className="brand-model">{`${ota.offer.brand} ${ota.offer.model}`}</span>
-                <span className="status">{ota.offer.status}</span>
-                <div className="applications">
+                <div className="top" onClick={(e) => toggleClass(e)}>
+                    <span className="title">{ota.offer.title}</span>
+                    <div className="bottom">
+                        <span className="type">{ota.offer.type}</span>
+                        <span className="brand-model">{`${ota.offer.brand} ${ota.offer.model}`}</span>
+                    </div>
+                </div>
+                {/* <span className="status">{ota.offer.status}</span> */}
+                <div className="applications disappear">
                     {ota.applications.map((a) => <Link to={`/applications/${a.application_id}/documents`}><div className="application">
                         <span className="name">{`${a.firstname.toUpperCase()} ${a.lastname.toUpperCase()}`}</span>
-                        <span className="state">{a.state}</span>
-                        <span className="date">{a.application_date}</span>
+                        <span className="date">{new Date(a.application_date).toLocaleDateString("fr-FR", {year: "numeric", month: "2-digit", day: "2-digit"}).split("/").reverse().join("-")}</span>
+                        <span className="state" style={a.state === "ACCEPTED" ? {color: 'var(--green-500)', borderColor: 'var(--green-500)'} : (a.state === "REJECTED" ? {color: 'var(--red-400)', borderColor: 'var(--red-400)'} : {})}>{a.state}</span>
                     </div></Link>)}
                 </div>
             </div>)}
