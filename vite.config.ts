d55b98e1ab5fec05cb4f25d1e6e09e8059348d0b
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-09-19 14:57:16 +0200

    Host set

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["https://bikedrivers-frontend-v1-0-1-1.onrender.com"]
+  }
 })
